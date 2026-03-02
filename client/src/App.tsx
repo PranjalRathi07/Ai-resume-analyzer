2d0902c80099bc136a609dc9be56a8284b414778
PranjalRathi07 <rathipranjal123@gmail.com>
2026-03-02 15:19:36 +0530

    create client in ts and use 21st.dev

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+/** @format */
+import { AppSidebar } from "./components/whatsapp-sidebar";
+import "./App.css";
+
+function App() {
+	return (
+		<div className='App bg-linear-to-br min-h-screen from-slate-900 via-slate-800 to-slate-900'>
+			<AppSidebar></AppSidebar>
+		</div>
+	);
+}
+
+export default App;
