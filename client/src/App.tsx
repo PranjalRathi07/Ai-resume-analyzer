f0a5c3c0b40d48b8f9f5f7f1413c217bea442acb
PranjalRathi07 <rathipranjal123@gmail.com>
2026-03-03 18:17:10 +0530

    get sidebar from 21st.dev

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 /** @format */
 import { AppSidebar } from "./components/whatsapp-sidebar";
+import { SidebarProvider } from "./components/sidebar";
 import "./App.css";
 
 function App() {
 	return (
 		<div className='App bg-linear-to-br min-h-screen from-slate-900 via-slate-800 to-slate-900'>
-			<AppSidebar></AppSidebar>
+			<SidebarProvider>
+				<AppSidebar></AppSidebar>
+			</SidebarProvider>
 		</div>
 	);
 }
